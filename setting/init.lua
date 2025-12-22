c1eec8a87c8e400be5814abb5cf36dbe56b3a9ed
KwatMDPhD <kwat.me@icloud.com>
2025-12-21 23:02:58 -0700

    Done with init.lua.

--- setting/init.lua
+++ setting/init.lua
@@ -2,128 +2,87 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.opt.termguicolors = true
-
 vim.opt.number = true
 vim.opt.signcolumn = "number"
-
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
-
 vim.opt.updatetime = 240
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
 
 vim.keymap.set("n", "<Leader>h", "<Cmd>nohlsearch<CR>")
 
 if vim.g.neovide then
-  vim.keymap.set("n", "<C-0>", function()
-    vim.g.neovide_scale_factor = 1
-  end)
-  vim.keymap.set("n", "<C-->", function()
-    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.16
-  end)
-  vim.keymap.set("n", "<C-=>", function()
-    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.16
-  end)
+    local sf = "neovide_scale_factor"
+    vim.keymap.set("n", "<Leader>1", function() vim.g[sf] = 1 end)
+    vim.keymap.set("n", "<Leader>0", function() vim.g[sf] = vim.g[sf] - 0.1 end)
+    vim.keymap.set("n", "<Leader>2", function() vim.g[sf] = vim.g[sf] + 0.1 end)
 end
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--branch=stable",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--branch=stable",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      view = { width = 56 },
-      filters = {
-        custom = { "^\\.git$" },
-      },
-    },
-    keys = {
-      { "<Leader>t", "<Cmd>NvimTreeToggle<CR>" },
-    },
-  },
-
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-      defaults = {
-        file_ignore_patterns = { "^.git/" },
-        path_display = { "smart" },
-      },
-      pickers = {
-        find_files = { hidden = true },
-        live_grep = {
-          additional_args = { "--hidden", "--glob", "!.git/*" },
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            view = { width = 64 },
+            filters = { custom = { "^\\.git$" } },
+            update_focused_file = { enable = true },
         },
-      },
+        keys = { { "<Leader>t", "<Cmd>NvimTreeToggle<CR>" } },
     },
-    keys = {
-      { "<Leader>b", "<cmd>Telescope buffers<cr>" },
-      { "<Leader>f", "<cmd>Telescope find_files<cr>" },
-      { "<Leader>r", "<cmd>Telescope live_grep<cr>" },
+
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {},
+        keys = {
+            { "<Leader>b", "<Cmd>Telescope buffers<CR>" },
+            { "<Leader>f", "<Cmd>Telescope find_files<CR>" },
+            { "<Leader>r", "<Cmd>Telescope live_grep<CR>" },
+        },
     },
-  },
 
-  {
-    "rebelot/kanagawa.nvim",
-    config = function()
-      vim.cmd.colorscheme("kanagawa")
-    end,
-  },
-
-  {
-    "dominikduda/vim_current_word",
-    config = function()
-      vim.g["vim_current_word#highlight_current_word"] = 0
-      vim.cmd.highlight("CurrentWordTwins guibg=#790505")
-    end,
-  },
-
-  { "lewis6991/gitsigns.nvim", config = true },
-
-  { "brenoprata10/nvim-highlight-colors", config = true },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "lua",
-        "html",
-        "markdown",
-        "markdown_inline",
-        "julia",
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
+    {
+        "folke/tokyonight.nvim",
+        config = function() vim.cmd.colorscheme("tokyonight") end,
     },
-  },
 
-  {
-    "jpalardy/vim-slime",
-    config = function()
-      vim.g.slime_target = "neovim"
-      vim.g.slime_bracketed_paste = 1
-      vim.g.slime_cell_delimiter = "# ---- #"
-    end,
-    keys = {
-      { "<Space><Space>", "<Plug>SlimeSendCell", mode = "n" },
-      { "<Space><Space>", "<Plug>SlimeRegionSend", mode = "x" },
+    "lewis6991/gitsigns.nvim",
+
+    {
+        "brenoprata10/nvim-highlight-colors",
+        opts = {},
     },
-  },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        opts = {
+            ensure_installed = { "bash", "lua", "html", "markdown", "markdown_inline", "julia" },
+            highlight = { enable = true },
+            indent = { enable = true },
+        },
+    },
+
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        opts = {
+            open_mapping = "<Leader>s",
+            size = function() return math.floor(vim.o.lines * 0.32) end,
+        },
+    },
 })
+
+vim.keymap.set({"n", "v"}, "<Space><Space>", "<Cmd>ToggleTermSendVisualLines<CR>")
